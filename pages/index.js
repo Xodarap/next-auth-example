88a16a9aa37e0ed58251b7dcee7d60be87ee251f
Ben West <Xodarap@users.noreply.github.com>
2020-10-06 21:45:25 -0700

    signout

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,15 @@ import {
   signout
 } from 'next-auth/client'
 export default function Page () {
+  const [session, loading] = useSession()
   return (
     <Layout>
       <h1>NextAuth.js Example</h1>
       <p>
         This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
-      <button color="inherit" onClick={() => signin('google')}>Sign in</button>
+      {!session && <button color="inherit" onClick={() => signin('google')}>Sign in</button>}
+      {session && <button color="inherit" onClick={signout}>Sign out</button>}
     </Layout>
   )
 }
