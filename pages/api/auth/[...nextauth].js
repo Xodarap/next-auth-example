12c23ccae48206cb343327b7816c1d99fc03708a
Ben West <Xodarap@users.noreply.github.com>
2020-10-07 08:47:02 -0700

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,118 +1,113 @@
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+import Adapters from "next-auth/adapters"
+class User extends Adapters.TypeORM.Models.User.model {
+  // You can extend the options in a model but you should not remove the base
+  // properties or change the order of the built-in options on the constructor
+  constructor(name, email, image, emailVerified) {
+    super(name, email, image, emailVerified)
+  }
+}
 
-// For more information on each option (and a full list of options) go to
-// https://next-auth.js.org/configuration/options
+const UserSchema = {
+  name: "User",
+  target: User,
+  columns: {
+    ...Adapters.TypeORM.Models.User.schema.columns,
+    admin: {
+      type: "integer",
+      nullable: true,
+    },
+    pro: {
+      type: "integer",
+      nullable: true,
+    },
+  },
+}
+
+const UserWrapper = { model: User, schema: UserSchema }
+
+
 const options = {
-  // https://next-auth.js.org/configuration/providers
+  // Configure one or more authentication providers
   providers: [
-    Providers.Email({
-      server: process.env.EMAIL_SERVER, 
-      from: process.env.EMAIL_FROM,
-    }),
-    Providers.Apple({
-      clientId: process.env.APPLE_ID,
-      clientSecret: { 
-        appleId: process.env.APPLE_ID,
-        teamId: process.env.APPLE_TEAM_ID,
-        privateKey: process.env.APPLE_PRIVATE_KEY,
-        keyId: process.env.APPLE_KEY_ID,
-      }
-    }),
-    Providers.Auth0({
-      clientId: process.env.AUTH0_ID,
-      clientSecret: process.env.AUTH0_SECRET,
-      domain: process.env.AUTH0_DOMAIN
-    }),
-    Providers.Facebook({
-      clientId: process.env.FACEBOOK_ID,
-      clientSecret: process.env.FACEBOOK_SECRET
-    }),
-    Providers.GitHub({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET
-    }),
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
     }),
-    Providers.Twitter({
-      clientId: process.env.TWITTER_ID,
-      clientSecret: process.env.TWITTER_SECRET
-    }),
+    // ...add more providers here
   ],
-  // Database optional. MySQL, Maria DB, Postgres and MongoDB are supported.
-  // https://next-auth.js.org/configuration/database
-  //
-  // Notes:
-  // * You must to install an appropriate node_module for your database
-  // * The Email provider requires a database (OAuth providers do not)
-  database: process.env.DATABASE_URL,
 
-  // The secret should be set to a reasonably long random string.
-  // It is used to sign cookies and to sign and encrypt JSON Web Tokens, unless
-  // a seperate secret is defined explicitly for encrypting the JWT.
-  secret: process.env.SECRET,
+  // A database is optional, but required to persist accounts in a database
+  adapter: Adapters.TypeORM.Adapter(
+    // The first argument should be a database connection string or TypeORM config object
+    process.env.DATABASE_URL,
+    // The second argument can be used to pass custom models and schemas
+    {
+      models: {
+        User: UserWrapper
+      },
+    }),
+  callbacks: {
+    session: async (session, user, sessionToken) => {
+      try{
+        //if(!user || !session) { return Promise.resolve(session); }
+        session.admin = user.admin || (user.email == 'xodarap00@gmail.com')
+        session.beta = (user.email == 'xodarap00@gmail.com')
+        session.pro = user.pro
+        session.user_id = user.id
+        session.tt_warning = session.pro && (await userOk(user.id))
+      } catch(e) {
+        console.log('=== SESSION CHANGE ERROR ===')
+        console.log(e)
+      }
+      return Promise.resolve(session)
+    },
+    signIn: async (user, account, profile) => {
+      try {
+        //first login
+        if(!user?.id) { return Promise.resolve(true) }
 
-  session: {
-    // Use JSON Web Tokens for session instead of database sessions.
-    // This option can be used with or without a database for users/accounts.
-    // Note: `jwt` is automatically set to `true` if no database is specified.
-    jwt: true, 
-    
-    // Seconds - How long until an idle session expires and is no longer valid.
-    // maxAge: 30 * 24 * 60 * 60, // 30 days
+        class AuthenticationEvent extends Model {
+          static get tableName() {
+              return 'tiktok_next.authentication_events';
+          }
+        }
 
-    // Seconds - Throttle how frequently to write to database to extend a session.
-    // Use it to limit write operations. Set to 0 to always update the database.
-    // Note: This option is ignored if using JSON Web Tokens 
-    // updateAge: 24 * 60 * 60, // 24 hours
+        return AuthenticationEvent.query().insert({
+          'user_id': user.id,
+          'event': 'signin'
+        }).then(() => true).catch(() => true)
+      } catch (e) {
+        console.log('=== LOGIN ERROR ===')
+        console.log(e)
+        return Promise.resolve(true);
+      }
+    }
   },
-
-  // JSON Web tokens are only used for sessions if the `jwt: true` session
-  // option is set - or by default if no database is specified.
-  // https://next-auth.js.org/configuration/options#jwt
-  jwt: {
-    // A secret to use for key generation (you should set this explicitly)
-    // secret: 'INp8IvdIyeMcoGAgFGoA61DdBglwwSqnXJZkgz8PSnw', 
-    
-    // Set to true to use encryption (default: false)
-    // encryption: true,
-
-    // You can define your own encode/decode functions for signing and encryption
-    // if you want to override the default behaviour.
-    // encode: async ({ secret, token, maxAge }) => {},
-    // decode: async ({ secret, token, maxAge }) => {},
-  },
-
-  // You can define custom pages to override the built-in pages.
-  // The routes shown here are the default URLs that will be used when a custom
-  // pages is not specified for that route.
-  // https://next-auth.js.org/configuration/pages
   pages: {
-    // signIn: '/api/auth/signin',  // Displays signin buttons
-    // signOut: '/api/auth/signout', // Displays form with sign out button
-    // error: '/api/auth/error', // Error code passed in query string as ?error=
-    // verifyRequest: '/api/auth/verify-request', // Used for check email page
-    // newUser: null // If set, new users will be directed here on first sign in
+    newUser: '/authentication/new_user'
   },
+  events: {
+    createUser: createProfile
+  }
+}
 
-  // Callbacks are asynchronous functions you can use to control what happens
-  // when an action is performed.
-  // https://next-auth.js.org/configuration/callbacks 
-  callbacks: { 
-    // signIn: async (user, account, profile) => { return Promise.resolve(true) },
-    // redirect: async (url, baseUrl) => { return Promise.resolve(baseUrl) },
-    // session: async (session, user) => { return Promise.resolve(session) },
-    // jwt: async (token, user, account, profile, isNewUser) => { return Promise.resolve(token) }
-  },
-
-  // Events are useful for logging
-  // https://next-auth.js.org/configuration/events
-  events: { },
-
-  // Enable debug messages in the console if you are having problems
-  debug: false,
+async function createProfile(message) {
+  try {
+  const randomChoice = (arr) => arr[Math.floor(arr.length * Math.random())];
+  const words = ['stretching', 'his', 'hand', 'up', 'to', 'reach', 'the', 'stars', 'too', 'often', 'man', 'forgets', 'the', 'flowers', 'at', 'his', 'feet']
+  const invite_code = randomChoice(words) + randomChoice(words) + randomChoice(words)
+  await pg.raw(`insert into tiktok_next.profiles(
+    user_id, email, name, invite_code)
+    VALUES (?, ?, ?, ?)
+    on conflict do nothing`,
+    [message.id, message.email, message.name, invite_code])
+  } catch (e) {
+    console.log('=== event ERROR ===')
+    console.log(e)
+    return Promise.resolve(true);
+  }
 }
 
 export default (req, res) => NextAuth(req, res, options)
