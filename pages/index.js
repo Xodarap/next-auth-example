4d11b1d00d0b78a11064a545b7fa8d96a793e85c
Ben West <Xodarap@users.noreply.github.com>
2020-10-06 21:33:41 -0700

    test signin

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,9 @@
 import Layout from '../components/layout'
-
+import {
+  useSession,
+  signin,
+  signout
+} from 'next-auth/client'
 export default function Page () {
   return (
     <Layout>
@@ -7,6 +11,7 @@ export default function Page () {
       <p>
         This is an example site to demonstrate how to use <a href={`https://next-auth.js.org`}>NextAuth.js</a> for authentication.
       </p>
+      <button color="inherit" onClick={() => signin('google')}>Sign in</button>
     </Layout>
   )
 }
